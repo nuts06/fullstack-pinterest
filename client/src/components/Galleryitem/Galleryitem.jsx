45fa65c47cfb2ba72e5b92faba4916d695a7cb00
Natasha <121818639+nuts06@users.noreply.github.com>
2025-03-20 14:36:28 +0530

    added image kit library

--- client/src/components/Galleryitem/Galleryitem.jsx
+++ client/src/components/Galleryitem/Galleryitem.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import './galleryitem.css'
 import { Link } from 'react-router'
-
+import { IKImage } from 'imagekitio-react';
 
 const Galleryitem = ({item}) => {
   return (
     <div className='galleryItem' style={{gridRowEnd:`span ${Math.ceil(item.height/100)}`}}>
       {/* showing all the images from the component */}
-      <img src={item.media} alt="" />
+      {/* <img src={item.media} alt="" /> */}
+      {/* using image kit */}
+      <IKImage urlEndpoint={import.meta.env.VITE_URL_IK_ENDPOINT} path={item.media}/>
       <Link to={`/pin/${item.id}`} className="overlay" />
       {/* button for each image */}
       <button className='saveButton'>Save</button>
