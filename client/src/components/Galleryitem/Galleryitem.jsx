dcc4dfa321cbafaa2ef99a28a4f43c3f5e1a80e3
Natasha <121818639+nuts06@users.noreply.github.com>
2025-03-20 11:10:45 +0530

    design the card

--- client/src/components/Galleryitem/Galleryitem.jsx
+++ client/src/components/Galleryitem/Galleryitem.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './galleryitem.css'
+import { Link } from 'react-router'
+
+
 const Galleryitem = ({item}) => {
   return (
     <div className='galleryItem' style={{gridRowEnd:`span ${Math.ceil(item.height/100)}`}}>
-        <img src={item.media} alt="" />
+      {/* showing all the images from the component */}
+      <img src={item.media} alt="" />
+      <Link to={`/pin/${item.id}`} className="overlay" />
+      {/* button for each image */}
+      <button className='saveButton'>Save</button>
+        <div className="overlayIcons">
+          <button>
+            <img src="/general/share.svg" alt="" />
+          </button> 
+          <button>
+            <img src="/general/more.svg" alt="" />
+          </button> 
+        </div>
     </div>
   )
 }
